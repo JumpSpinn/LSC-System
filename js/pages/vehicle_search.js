ef77952adb14db455780af5911fd0b09437fb423
JumpSpinn <jumpspinn@gmx.de>
2023-01-16 18:53:42 +0100

    first steps for search vehicle by numberplate

--- js/pages/vehicle_search.js
+++ js/pages/vehicle_search.js
@@ -1,3 +1,30 @@
+var _searchedVehicles = []
+var _requestSearch = false
+
 $(() => {
-    
+    initSearchVehicle()
+
+    $('#search_vehicle_confirm').click(() => {
+        let numberplate = $('#search_vehicle_numberplate').val()
+        if(numberplate == ""){
+            new GNWX_NOTIFY({ text: "Bitte gebe ein gültiges Kennzeichen an!", position: "bottom-left", class: "gnwx-warning", autoClose: 5000 });   
+            return
+        }
+        searchVehicleByNumberplate(numberplate)
+        toggleLoading(true)
+    })
 })
+
+function initSearchVehicle(){
+    $('#search_vehicle_numberplate').val('')
+    $('#modal_search_vehicle').css('display', 'flex')
+    setTimeout(() => {
+        $('#search_vehicle_numberplate').focus()
+    }, 150);
+}
+
+function searchVehicleByNumberplate(numberplate){
+    if(_requestSearch) return
+    _requestSearch = true
+    _searchedVehicles = []
+}
