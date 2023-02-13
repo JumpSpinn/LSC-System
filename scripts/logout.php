9c6c9a40e00672ea51a298e6e216083a57ba75e1
JumpSpinn <jumpspinn@gmx.de>
2023-02-13 16:25:02 +0100

    remove cookie from db

--- scripts/logout.php
+++ scripts/logout.php
@@ -3,6 +3,14 @@
 
     if(isset($_COOKIE['LOGGEDIN'])){
         setcookie("LOGGEDIN", "", time() - 3600);
+
+        $value = "";
+        $stmt2 = $con->prepare("UPDATE employees SET `stateReason`=? WHERE id=?");
+        $stmt2->bind_param("si", $value, $_SESSION['accountID']);
+        if(!$stmt2->execute()){
+            echo 0;
+        }
+        $stmt2->close();
     }
     
     session_destroy();
