8100d767d0765be8f54fab82d856fd6cfb3532cd
JumpSpinn <jumpspinn@gmx.de>
2023-02-13 16:50:48 +0100

    next test

--- scripts/getSessionData.php
+++ scripts/getSessionData.php
@@ -5,32 +5,30 @@
     session_set_cookie_params(time() + (86400 * 7));
     session_start();
 
-    if(isset($_COOKIE['LOGGEDIN'])){
-        $stmt = $con->prepare("SELECT * FROM employees WHERE token=?");
-        $stmt->bind_param("s", $_COOKIE['LOGGEDIN']);
-    
-        if($stmt->execute()){
-            $result = $stmt->get_result();
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    if($row["state"] == 4){
-                        echo 0;
-                    } else {
-                        $_SESSION['accountID'] = $row["id"];
-                        $_SESSION['firstname'] = $row["firstname"];
-                        $_SESSION['lastname'] = $row["lastname"];
-                        $_SESSION['currentSidebarID'] = $row["currentSidebarID"];
-                        $_SESSION['currentSubSidebarID'] = $row["currentSubSidebarID"];
-                        $_SESSION['positionID'] = $row["positionID"];
-                        echo $row['id'] . "_" . $row['firstname'] . "_" . $row['lastname'] . "_" . $row['currentSidebarID'] . "_" . $row['currentSubSidebarID'] . "_" . $row['positionID'];
-                    }
+    $stmt = $con->prepare("SELECT * FROM employees WHERE token=?");
+    $stmt->bind_param("s", $_COOKIE['LOGGEDIN']);
+
+    if($stmt->execute()){
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                if($row["state"] == 4){
+                    echo 0;
+                } else {
+                    $_SESSION['accountID'] = $row["id"];
+                    $_SESSION['firstname'] = $row["firstname"];
+                    $_SESSION['lastname'] = $row["lastname"];
+                    $_SESSION['currentSidebarID'] = $row["currentSidebarID"];
+                    $_SESSION['currentSubSidebarID'] = $row["currentSubSidebarID"];
+                    $_SESSION['positionID'] = $row["positionID"];
+                    echo $row['id'] . "_" . $row['firstname'] . "_" . $row['lastname'] . "_" . $row['currentSidebarID'] . "_" . $row['currentSubSidebarID'] . "_" . $row['positionID'];
                 }
-            } else {
-                echo 0;
             }
         } else {
             echo 0;
         }
-        $stmt->close();
+    } else {
+        echo 0;
     }
+    $stmt->close();
 ?>
